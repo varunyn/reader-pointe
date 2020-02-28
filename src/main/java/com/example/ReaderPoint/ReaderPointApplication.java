4ad3e94a68f846f94462a3d3b8186376ce7d40ca
jitendra <jitendra.vagdoda@sprinklr.com>
2020-02-27 18:42:25 -0600

    First commit

---
+++ src/main/java/com/example/ReaderPoint/ReaderPointApplication.java
@@ -0,0 +1,14 @@
+package com.example.ReaderPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderPointApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ReaderPointApplication.class, args);
+	}
+
+}
